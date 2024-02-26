68c6f58d0d565aba9dc0db99a28c4336c8cb2bb7
lucazcruz <lucaz.cruz17@gmail.com>
2024-02-26 15:01:46 -0300

    fix: sounds dont working

--- script/sound.js
+++ script/sound.js
@@ -1,7 +1,7 @@
 export default function Sound() {
-  const pressSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/button-press.wav');
-  const endSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/timer-end.wav');
-  const bgSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/background.wav');
+  const pressSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/button-press.wav?raw=true');
+  const endSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/timer-end.wav?raw=true');
+  const bgSound = new Audio('https://github.com/lucazcruz/timer/blob/main/assets/music/background.wav?raw=true');
 
   return {
     pressSound,
